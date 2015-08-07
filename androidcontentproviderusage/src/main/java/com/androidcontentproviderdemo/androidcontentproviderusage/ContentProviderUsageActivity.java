4e0a02e6ce8d16e5e29ba6e27d91ec611dcee507
Abbas Suterwala <abbas.s786@yahoo.co.in>
2015-08-07 09:11:58 +0530

    Fixed some bugs to fetch values from the content provider and display on the list view.

--- androidcontentproviderusage/src/main/java/com/androidcontentproviderdemo/androidcontentproviderusage/ContentProviderUsageActivity.java
+++ androidcontentproviderusage/src/main/java/com/androidcontentproviderdemo/androidcontentproviderusage/ContentProviderUsageActivity.java
@@ -6,6 +6,7 @@ import android.content.CursorLoader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -27,19 +28,20 @@ public class ContentProviderUsageActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content_provider_usage);
+
         listView = (ListView) findViewById(R.id.lstViewImages);
 
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(getBaseContext(),
+        adapter = new SimpleCursorAdapter(getBaseContext(),
                 R.layout.list_layout,
                 null,
                 new String[] { "IMAGETITLE", "IMAGEURL", "IMAGEDESC"},
                 new int[] { R.id.imgTitle , R.id.imgUrl, R.id.imgDesc }, 0);
 
         listView.setAdapter(adapter);
-        fetchValuesFromContentProvider();
+        refreshValuesFromContentProvider();
     }
 
-    private void fetchValuesFromContentProvider() {
+    private void refreshValuesFromContentProvider() {
         CursorLoader cursorLoader = new CursorLoader(getBaseContext(), CONTENT_URI,
                 null, null, null, null);
         Cursor c = cursorLoader.loadInBackground();
@@ -53,28 +55,13 @@ public class ContentProviderUsageActivity extends Activity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     public void onClickAddImage(View view) {
         ContentValues contentValues = new ContentValues();
-        contentValues.put("IMAGETITLE" , ((EditText)findViewById(R.id.edtTxtImageTitle)).getText().toString());
+        contentValues.put("IMAGETITLE", ((EditText) findViewById(R.id.edtTxtImageTitle)).getText().toString());
         contentValues.put("IMAGEURL" , ((EditText)findViewById(R.id.edtImageUrl)).getText().toString());
-        contentValues.put("IMAGEDESC" , ((EditText)findViewById(R.id.edtImageDesc)).getText().toString());
+        contentValues.put("IMAGEDESC", ((EditText) findViewById(R.id.edtImageDesc)).getText().toString());
         Uri uri = getContentResolver().insert(CONTENT_URI, contentValues);
         Toast.makeText(getBaseContext(), uri.toString(), Toast.LENGTH_LONG).show();
-
+        refreshValuesFromContentProvider();
     }
 }
